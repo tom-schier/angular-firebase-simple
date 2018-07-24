1c26a25bdf2de22ac6029b6756a728c0c2f2cd8a
tom-schier <schiertxs@gmail.com>
2018-07-24 21:50:55 +1000

    Updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { Observable } from 'rxjs';
+import { AngularFireAuth } from '../../node_modules/angularfire2/auth';
+import { auth } from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +12,7 @@ import { Observable } from 'rxjs';
 export class AppComponent {
   items: Observable<any[]>;
   _db: AngularFirestore
-  constructor(db: AngularFirestore) {
+  constructor(db: AngularFirestore, public afAuth: AngularFireAuth) {
     this._db = db;
     this.items = db.collection('Locations').valueChanges();
   }
@@ -19,5 +21,12 @@ export class AppComponent {
     this._db.collection("Locations").doc("02daaddb-23a6-433b-87fd-cc4b177a4278").valueChanges().subscribe(data => {
         console.log("Yeah then " + data);
     })
+  }
+
+  login() {
+    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+  logout() {
+    this.afAuth.auth.signOut();
   }
 }
